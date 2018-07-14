92e723e451423c4981129da2acbebc602d0cdae4
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2018-07-14 23:19:53 +0200

    added tomcat install

--- ubuntu.install.sh
+++ ubuntu.install.sh
@@ -15,7 +15,7 @@ echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selec
 echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
 
 # Package-install
-apt-get install -y git vim ufw google-chrome-stable maven dkms build-essential unity-tweak-tool virtualbox oracle-java8-jdk oracle-java8-installer oracle-java8-set-default
+apt-get install -y unzip wget git vim ufw google-chrome-stable maven dkms build-essential unity-tweak-tool virtualbox oracle-java8-jdk oracle-java8-installer oracle-java8-set-default
 
 # Git-config
 git config --global push.default simple
@@ -35,3 +35,16 @@ ufw default allow outgoing
 ufw allow ssh
 sed -i.bak 's/ENABLED=no/ENABLED=yes/g' /etc/ufw/ufw.conf
 chmod 0644 /etc/ufw/ufw.conf
+
+# Tomcat
+useradd -m -U -d /opt/tomcat -s /bin/false tomcat
+wget --directory-prefix=/tmp/ http://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.32/bin/apache-tomcat-8.5.32.zip
+unzip /tmp/apache-tomcat-8.5.32.zip
+mv /tmp/apache-tomcat-8.5.32/ /opt/tomcat/
+ln -s /opt/tomcat/apache-tomcat-8.5.32 /opt/tomcat/latest
+chown -R tomcat: /opt/tomcat
+chmod +x /opt/tomcat/latest/bin/*.sh
+cp ./tomcat.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
