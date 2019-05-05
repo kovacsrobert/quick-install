ee69b99c21d146da92a319fac26b27b18f4a6738
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2019-05-05 07:54:41 +0200

    docker install updated

--- docker.install.sh
+++ docker.install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-curl -sSL https://get.docker.com | sh
-usermod -aG docker pi
+# Uninstall old versions
+apt remove docker docker-engine docker.io containerd runc
 
-printf '\n' >> ~/.bashrc
-printf 'DOCKER_HOST="https://hub.docker.com/"' >> ~/.bashrc
-printf '\n' >> ~/.bashrc
+# Install packages to allow apt to use a repository over HTTPS
+apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Install the latest version of Docker CE and containerd
+apt update && apt install docker-ce docker-ce-cli containerd.io
+
+# Add user to docker group
+# usermod -aG docker pi
+
+# printf '\n' >> ~/.bashrc
+# printf 'DOCKER_HOST="https://hub.docker.com/"' >> ~/.bashrc
+# printf '\n' >> ~/.bashrc
