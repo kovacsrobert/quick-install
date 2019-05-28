3c1ebf11882a4b0b8671440a2d40905a6ddfc52f
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2019-05-28 17:37:50 +0200

    fix docker install

--- docker.install.sh
+++ docker.install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Uninstall old versions
-apt remove docker docker-engine docker.io containerd runc
+apt remove docker docker-engine docker.io containerd runc docker-compose
 
 # Install packages to allow apt to use a repository over HTTPS
 apt install \
@@ -13,6 +13,11 @@ apt install \
 
 # Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+# Add stable repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 # Install the latest version of Docker CE and containerd
 apt update && apt install docker-ce docker-ce-cli containerd.io
